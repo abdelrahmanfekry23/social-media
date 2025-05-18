413e7f2865855432677f39ac7868c9d2e3f4510e
abdelrahmanfekry23 <abdelrahmanmfekry@gmail.com>
2025-05-18 10:02:33 +0300

    fix: exclude reduxjs test files to avoid MSW error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // استخدم SWC بدل Babel للتصغير
+ // swcMinify: true, // استخدم SWC بدل Babel للتصغير
     eslint: {
     ignoreDuringBuilds: true,
   },
